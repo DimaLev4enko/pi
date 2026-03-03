80f23a76271b5e283254cf2898d2f06a036925aa
DimaT <dimalevenko6@gmail.com>
2026-03-03 15:56:06 +0200

    Готов к переносу на ноутбук

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+use std::io;
 fn main() {
-let c =4.0;
+let mut buffer = String::new();
+    println!("Enter method:\n1.Slow\n2.Not now");
+let choice = loop {
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).expect("Ошибка");
+    if let Ok(num) = buffer.trim().parse::<u8>() {
+            if num > 0 && num <= 1 {
+                break num;
+            } else {
+                println!("Wrong number");
+            }
+        } else {
+            println!("Not number");
+        }
+
+};
+println!("Enter itteraion");
+let itter = loop {
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).expect("Error");
+    if let Ok(num) = buffer.trim().parse::<u128>() {
+        if num > 0 && num <= u128::MAX {
+            break num;
+            }
+        } else {
+            println!("Wrong number");
+        }
+    } else {
+    println!("Not number");
+    };
+/*let c =4.0;
  let mut pi = 0.0;
 let mut b = 1.0;
     let mut plus: bool =true;
@@ -15,5 +46,5 @@ let mut b = 1.0;
         if i % 100000000 == 0 {
             println!("число пи {}", pi);
         }  
-    }
+    }*/
 }
