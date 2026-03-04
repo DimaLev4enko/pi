9419aa1638718246048855d4526e325d409872f3
Dima <dimalevenko6@gmail.com>
2026-03-04 12:07:07 +0200

    feat: add high-precision sine calculator using Taylor series

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use dashu::float::DBig;
 use std::io;
 use std::time::Instant;
+
+fn my_pow(main: DBig, exp: u128, percision: usize) -> DBig {
+    let mut res = DBig::from(1u32).with_precision(percision).value();
+    for _ in 1..=exp {
+        res *= &main;
+    }
+    res
+}
+
 fn main() {
     let mut buffer = String::new();
     println!(
-        "Enter method:\n1.Slow\n2.Fast f64\n3.Fast Big\n4.Euler big\n5.Golden ratio big super fast sqrt(5)"
+        "Enter method:\n1.Slow\n2.Fast f64\n3.Fast Big\n4.Euler big\n5.Golden ratio big super fast sqrt(5)\n6.cos(x)"
     );
     let choice = loop {
         buffer.clear();
         io::stdin().read_line(&mut buffer).expect("Ошибка");
         if let Ok(num) = buffer.trim().parse::<u8>() {
-            if num > 0 && num <= 5 {
+            if num > 0 && num <= 6 {
                 break num;
             } else {
                 println!("Wrong number");
@@ -48,7 +57,7 @@ fn main() {
         }
     };
     let mut precision = 1;
-    if choice == 3 || choice == 4 || choice == 5 {
+    if choice == 3 || choice == 4 || choice == 5 || choice == 6 {
         println!("Введите точность: (желательно не больше 1000");
         precision = loop {
             buffer.clear();
@@ -156,6 +165,40 @@ fn main() {
         }
         let phi = (&one + &x) / &two;
         println!("Golden ratio: {}", phi);
+    } else if choice == 6 {
+        println!("Enter x: ");
+        let x = loop {
+            buffer.clear();
+            io::stdin().read_line(&mut buffer).expect("Error");
+            if let Ok(num) = buffer.trim().parse::<DBig>() {
+                break num.with_precision(precision).value();
+            } else {
+                println!("Это не число! Давай еще раз:");
+            }
+        };
+        let mut fac = DBig::from(6u32).with_precision(precision).value();
+        let mut plus = false;
+        let mut sinx = x.clone();
+        let mut k = 3u128;
+        for i in 0..=itter {
+            let powx = my_pow(x.clone(), k, precision);
+            if plus {
+                plus = false;
+                sinx += powx / &fac;
+            } else {
+                plus = true;
+                sinx -= powx / &fac;
+            }
+            k += 1;
+            fac *= k;
+            k += 1;
+            fac *= k;
+            if i == itter {
+                println!("Финальный ответ sin(x): {}", sinx);
+            } else if i & info == 0 {
+                println!("Расщет sin(x): {}", sinx);
+            }
+        }
     }
     let duration = start.elapsed();
     println!("Время: {:?}", duration);
